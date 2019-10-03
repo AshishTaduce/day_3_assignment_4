9e3a8e1b70ac96de5595f081e4d1822689f2f488
AshishTaduce <ashishteddy2@gmail.com>
2019-10-03 14:35:02 +0530

    Complete wittout Stretch

--- lib/main.dart
+++ lib/main.dart
@@ -15,113 +15,145 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //appBar: AppBar(
-        //title: Text('Strawberry Pavlova'),
+      //title: Text('Strawberry Pavlova'),
       //),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Strawberry Pavlova',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+      body: Container(
+        color: Colors.pink,
+        child: Row(
 
-                    ),
-                  ),
-
-                  Text(
-                    'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova.[1] It is a meringue dessert with a crisp crust and soft, light inside, usually topped with fruit and whipped cream.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-
-                  Container(
-                    child: Row(
-                      children: <Widget>[
-
-                        Row(
-
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Icon(
-                              Icons.star_border,
-                              color: Colors.grey,
-                            ),
-                            Icon(
-                              Icons.star_border,
-                              color: Colors.grey,
-                            ),
-                            Icon(
-                              Icons.star_border,
-                              color: Colors.grey,
-                            ),
-                            Icon(
-                              Icons.star_border,
-                              color: Colors.grey,
-                            ),
-                            Icon(
-                              Icons.star_border,
-                              color: Colors.grey,
-                            ),
-                          ],
-                        ),
-
-                        Text(
-                          '170 Reviews',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       Text(
                         'Strawberry Pavlova',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
+                      ),
+                      Text(
+                        'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova.[1] It is a meringue dessert with a crisp crust and soft, light inside, usually topped with fruit and whipped cream.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.star_border,
+                                  color: Colors.white,
+                                ),
+                                Icon(
+                                  Icons.star_border,
+                                  color: Colors.white,
+                                ),
+                                Icon(
+                                  Icons.star_border,
+                                  color: Colors.white,
+                                ),
+                                Icon(
+                                  Icons.star_border,
+                                  color: Colors.white,
+                                ),
+                                Icon(
+                                  Icons.star_border,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                            Text(
+                              '170 Reviews',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Icon(
+                                Icons.check_circle_outline,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'PREP \n 25 mins',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Icon(
+                                Icons.timer,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'COOK \n 1-2 hrs',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Icon(
+                                Icons.person,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'FEED \n 4=5 people',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-          ),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: FittedBox(
-              child: Image(
-                image: AssetImage(
-                  'assets/graphics/Strawberry_Pavalov.png',
                 ),
-                fit: BoxFit.fill,
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              child: FittedBox(
+                child: Image(
+                  image: AssetImage(
+                    'assets/graphics/Strawberry_Pavalov.png',
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
-
   }
 }
